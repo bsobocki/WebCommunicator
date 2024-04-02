2dfb604de1d89d75f33f1506280e0a70f5bb0105
bsobocki <bartq98@gmail.com>
2024-04-02 05:30:05 +0200

    name changing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,22 +11,6 @@ function App() {
       <AppLayout appBar={<AppBar/>} navigation={<Sidebar/>}>
         <Chat title='Andrzejek Tate' messages={andrzejekMessages} />
       </AppLayout>
-
-    {/* <div style={{background: 'red'}}>
-      <div >
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-   </div> */}
    </AppContainer>
 
   )
